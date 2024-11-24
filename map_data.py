d68dda92582f116f6c02d9178933c01decb52af3
jaejoonlee <lhw5922@gmail.com>
2024-11-24 21:18:27 +0900

    맵 섹션 6까지 블럭 추가

--- map_data.py
+++ map_data.py
@@ -1,18 +1,80 @@
 # 블럭 및 바닥 데이터 정의
 map_data = [
+
+    # section 1
+    { "x": 0, "y": 0, "type": "invisible", "width": 800, "height": 100 },
+
+    { "x": 640, "y": 210, "type": "question_block", "width": 40, "height": 40 },
+
+    # section 2
+    { "x": 800, "y": 0, "type": "invisible", "width": 1600, "height": 100 },
+
     { "x": 800, "y": 210, "type": "block", "width": 40, "height": 40 },
     { "x": 880, "y": 210, "type": "block", "width": 40, "height": 40 },
     { "x": 960, "y": 210, "type": "block", "width": 40, "height": 40 },
-    { "x": 640, "y": 210, "type": "question_block", "width": 40, "height": 40 },
     { "x": 840, "y": 210, "type": "question_block", "width": 40, "height": 40 },
     { "x": 920, "y": 210, "type": "question_block", "width": 40, "height": 40 },
     { "x": 880, "y": 370, "type": "question_block", "width": 40, "height": 40 },
+
     { "x": 1120, "y": 87, "type": "pipe", "width": 80, "height": 85 },
     { "x": 1518, "y": 87, "type": "pipe", "width": 80, "height": 130 },
 
-    # 보이지 않는 바닥 사각형
-    { "x": 0, "y": 0, "type": "invisible", "width": 800, "height": 100 },
-    { "x": 800, "y": 0, "type": "invisible", "width": 1600, "height": 100 },
+    # section 3
     { "x": 1600, "y": 0, "type": "invisible", "width": 2400, "height": 100 },
-    { "x": 2400, "y": 0, "type": "invisible", "width": 3000, "height": 100 }
+
+    { "x": 1840, "y": 87, "type": "pipe", "width": 80, "height": 165 },
+    { "x": 2280, "y": 87, "type": "pipe", "width": 80, "height": 165 },
+
+    # section 4
+    { "x": 2400, "y": 0, "type": "invisible", "width": 3000, "height": 100 },
+
+    { "x": 3080, "y": 210, "type": "block", "width": 40, "height": 40 },
+    { "x": 3160, "y": 210, "type": "block", "width": 40, "height": 40 },
+    { "x": 3120, "y": 210, "type": "question_block", "width": 40, "height": 40 },
+
+    # section 5
+    { "x": 3200, "y": 0, "type": "invisible", "width": 3000, "height": 100 },
+
+    { "x": 3200, "y": 370, "type": "block", "width": 40, "height": 40 },
+    { "x": 3240, "y": 370, "type": "block", "width": 40, "height": 40 },
+    { "x": 3280, "y": 370, "type": "block", "width": 40, "height": 40 },
+    { "x": 3320, "y": 370, "type": "block", "width": 40, "height": 40 },
+    { "x": 3360, "y": 370, "type": "block", "width": 40, "height": 40 },
+    { "x": 3400, "y": 370, "type": "block", "width": 40, "height": 40 },
+    { "x": 3440, "y": 370, "type": "block", "width": 40, "height": 40 },
+    { "x": 3480, "y": 370, "type": "block", "width": 40, "height": 40 },
+
+    { "x": 3640, "y": 370, "type": "block", "width": 40, "height": 40 },
+    { "x": 3680, "y": 370, "type": "block", "width": 40, "height": 40 },
+    { "x": 3720, "y": 370, "type": "block", "width": 40, "height": 40 },
+    { "x": 3760, "y": 370, "type": "question_block", "width": 40, "height": 40 },
+
+    { "x": 3760, "y": 210, "type": "block", "width": 40, "height": 40 },
+
+    # section 6
+    { "x": 4000, "y": 0, "type": "invisible", "width": 3000, "height": 100 },
+
+    { "x": 4000, "y": 210, "type": "block", "width": 40, "height": 40 },
+    { "x": 4040, "y": 210, "type": "block", "width": 40, "height": 40 },
+
+    { "x": 4240, "y": 210, "type": "question_block", "width": 40, "height": 40 },
+    { "x": 4360, "y": 210, "type": "question_block", "width": 40, "height": 40 },
+    { "x": 4360, "y": 370, "type": "question_block", "width": 40, "height": 40 },
+    { "x": 4480, "y": 210, "type": "question_block", "width": 40, "height": 40 },
+
+    { "x": 4720, "y": 210, "type": "block", "width": 40, "height": 40 },
+
+
+
+    # section 7
+    { "x": 4800, "y": 0, "type": "invisible", "width": 3000, "height": 100 },
+
+    # section 8
+    { "x": 5600, "y": 0, "type": "invisible", "width": 3000, "height": 100 },
+
+    # section 9
+    { "x": 6400, "y": 0, "type": "invisible", "width": 3000, "height": 100 },
+
+    # section 10
+    { "x": 7200, "y": 0, "type": "invisible", "width": 3000, "height": 100 }
 ]
