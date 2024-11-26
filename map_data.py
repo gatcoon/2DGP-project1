1d5e71337154243291d48bfecb191d9ef0c6b699
jaejoonlee <lhw5922@gmail.com>
2024-11-26 16:51:23 +0900

    맵 낭떨어지 추가/ 땅 높이 조절

--- map_data.py
+++ map_data.py
@@ -2,12 +2,12 @@
 map_data = [
 
     # section 1
-    { "x": 0, "y": 0, "type": "invisible", "width": 800, "height": 100 },
+    { "x": 0, "y": 0, "type": "invisible", "width": 800, "height": 90 },
 
     { "x": 640, "y": 210, "type": "question_block", "width": 40, "height": 40 },
 
     # section 2
-    { "x": 800, "y": 0, "type": "invisible", "width": 1600, "height": 100 },
+    { "x": 800, "y": 0, "type": "invisible", "width": 1600, "height": 90 },
 
     { "x": 800, "y": 210, "type": "block", "width": 40, "height": 40 },
     { "x": 880, "y": 210, "type": "block", "width": 40, "height": 40 },
@@ -20,20 +20,24 @@ map_data = [
     { "x": 1518, "y": 87, "type": "pipe", "width": 80, "height": 130 },
 
     # section 3
-    { "x": 1600, "y": 0, "type": "invisible", "width": 2400, "height": 100 },
+    { "x": 1600, "y": 0, "type": "invisible", "width": 2400, "height": 90 },
 
     { "x": 1840, "y": 87, "type": "pipe", "width": 80, "height": 165 },
     { "x": 2280, "y": 87, "type": "pipe", "width": 80, "height": 165 },
 
     # section 4
-    { "x": 2400, "y": 0, "type": "invisible", "width": 3000, "height": 100 },
+    { "x": 2400, "y": 0, "type": "invisible", "width": 360, "height": 90 },
+    { "x": 2840, "y": 0, "type": "invisible", "width": 360, "height": 90 },
+
 
     { "x": 3080, "y": 210, "type": "block", "width": 40, "height": 40 },
     { "x": 3160, "y": 210, "type": "block", "width": 40, "height": 40 },
     { "x": 3120, "y": 210, "type": "question_block", "width": 40, "height": 40 },
 
     # section 5
-    { "x": 3200, "y": 0, "type": "invisible", "width": 3000, "height": 100 },
+    { "x": 3200, "y": 0, "type": "invisible", "width": 240, "height": 90 },
+    { "x": 3560, "y": 0, "type": "invisible", "width": 440, "height": 90 },
+
 
     { "x": 3200, "y": 370, "type": "block", "width": 40, "height": 40 },
     { "x": 3240, "y": 370, "type": "block", "width": 40, "height": 40 },
@@ -52,7 +56,7 @@ map_data = [
     { "x": 3760, "y": 210, "type": "block", "width": 40, "height": 40 },
 
     # section 6
-    { "x": 4000, "y": 0, "type": "invisible", "width": 3000, "height": 100 },
+    { "x": 4000, "y": 0, "type": "invisible", "width": 3000, "height": 90 },
 
     { "x": 4000, "y": 210, "type": "block", "width": 40, "height": 40 },
     { "x": 4040, "y": 210, "type": "block", "width": 40, "height": 40 },
@@ -67,7 +71,7 @@ map_data = [
 
 
     # section 7
-    { "x": 4800, "y": 0, "type": "invisible", "width": 3000, "height": 100 },
+    { "x": 4800, "y": 0, "type": "invisible", "width": 3000, "height": 90 },
 
     { "x": 4840, "y": 370, "type": "block", "width": 40, "height": 40 },
     { "x": 4880, "y": 370, "type": "block", "width": 40, "height": 40 },
@@ -91,7 +95,9 @@ map_data = [
     { "x": 5480, "y": 90, "type": "solid", "width": 40, "height": 40},
 
     # section 8
-    { "x": 5600, "y": 0, "type": "invisible", "width": 3000, "height": 100 },
+    { "x": 5600, "y": 0, "type": "invisible", "width": 520, "height": 90 },
+    { "x": 6200, "y": 0, "type": "invisible", "width": 200, "height": 90 },
+
 
     { "x": 5600, "y": 210, "type": "solid", "width": 40, "height": 40},
     { "x": 5640, "y": 170, "type": "solid", "width": 40, "height": 40},
@@ -118,7 +124,7 @@ map_data = [
     { "x": 6320, "y": 90, "type": "solid", "width": 40, "height": 40},
 
     # section 9
-    { "x": 6400, "y": 0, "type": "invisible", "width": 3000, "height": 100 },
+    { "x": 6400, "y": 0, "type": "invisible", "width": 3000, "height": 90 },
 
     { "x": 6720, "y": 210, "type": "block", "width": 40, "height": 40 },
     { "x": 6760, "y": 210, "type": "block", "width": 40, "height": 40 },
@@ -131,9 +137,9 @@ map_data = [
 
 
     # section 10
-    { "x": 7200, "y": 0, "type": "invisible", "width": 3000, "height": 100 },
+    { "x": 7200, "y": 0, "type": "invisible", "width": 3000, "height": 90 },
 
-    { "x": 7160, "y": 87, "type": "pipe", "width": 80, "height": 80 },
+    { "x": 7200, "y": 87, "type": "pipe", "width": 40, "height": 80 },
 
     { "x": 7240, "y": 90, "type": "solid", "width": 40, "height": 40 },
     { "x": 7280, "y": 130, "type": "solid", "width": 40, "height": 40 },
