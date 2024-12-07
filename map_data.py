7a9724fd1bac28842d0941b41e755f2fefc6e7a4
jaejoonlee <lhw5922@gmail.com>
2024-12-07 20:24:56 +0900

    맵에 코인 추가

--- map_data.py
+++ map_data.py
@@ -8,8 +8,9 @@ map_data = [
 
     { "x": 640, "y": 100, "type": "enemy" },
 
-    { "x": 300, "y": 150, "type": "coin" },
-    { "x": 400, "y": 180, "type": "coin" },
+    { "x": 400, "y": 200, "type": "coin" },
+    { "x": 450, "y": 250, "type": "coin" },
+    { "x": 500, "y": 200, "type": "coin" },
 
     # section 2
     { "x": 800, "y": 0, "type": "invisible", "width": 1600, "height": 90 },
@@ -29,6 +30,11 @@ map_data = [
 
     { "x": 900, "y": 150, "type": "powerup", "width": 40, "height": 40 },
 
+    { "x": 1140, "y": 200, "type": "coin" },
+    { "x": 1180, "y": 200, "type": "coin" },
+    { "x": 1538, "y": 300, "type": "coin" },
+    { "x": 1578, "y": 300, "type": "coin" },
+
     # section 3
     { "x": 1600, "y": 0, "type": "invisible", "width": 2400, "height": 90 },
 
@@ -37,6 +43,16 @@ map_data = [
 
     { "x": 2100, "y": 100, "type": "enemy" },
     { "x": 2200, "y": 100, "type": "enemy" },
+
+    { "x": 1700, "y": 150, "type": "coin" },
+    { "x": 1750, "y": 200, "type": "coin" },
+    { "x": 1800, "y": 250, "type": "coin" },
+    { "x": 1850, "y": 300, "type": "coin" },
+
+    { "x": 2140, "y": 150, "type": "coin" },
+    { "x": 2190, "y": 200, "type": "coin" },
+    { "x": 2240, "y": 250, "type": "coin" },
+    { "x": 2290, "y": 300, "type": "coin" },
 
     # section 4
     { "x": 2400, "y": 0, "type": "invisible", "width": 360, "height": 90 },
@@ -47,6 +63,11 @@ map_data = [
     { "x": 3080, "y": 210, "type": "block", "width": 40, "height": 40 },
     { "x": 3160, "y": 210, "type": "block", "width": 40, "height": 40 },
     { "x": 3120, "y": 210, "type": "question_block", "width": 40, "height": 40 },
+
+    { "x": 2720, "y": 200, "type": "coin" },
+    { "x": 2760, "y": 250, "type": "coin" },
+    { "x": 2800, "y": 250, "type": "coin" },
+    { "x": 2840, "y": 200, "type": "coin" },
 
     # section 5
     { "x": 3200, "y": 0, "type": "invisible", "width": 240, "height": 90 },
@@ -69,6 +90,16 @@ map_data = [
 
     { "x": 3760, "y": 210, "type": "block", "width": 40, "height": 40 },
 
+    { "x": 3440, "y": 200, "type": "coin" },
+    { "x": 3480, "y": 250, "type": "coin" },
+    { "x": 3520, "y": 250, "type": "coin" },
+    { "x": 3560, "y": 200, "type": "coin" },
+
+    { "x": 3520, "y": 520, "type": "coin" },
+    { "x": 3560, "y": 570, "type": "coin" },
+    { "x": 3600, "y": 570, "type": "coin" },
+    { "x": 3640, "y": 520, "type": "coin" },
+
     # section 6
     { "x": 4000, "y": 0, "type": "invisible", "width": 3000, "height": 90 },
 
@@ -87,6 +118,11 @@ map_data = [
     { "x": 4650, "y": 100, "type": "enemy" },
     { "x": 4700, "y": 100, "type": "enemy" },
     { "x": 4750, "y": 100, "type": "enemy" },
+
+    { "x": 4260, "y": 280, "type": "coin" },
+    { "x": 4380, "y": 280, "type": "coin" },
+    { "x": 4380, "y": 450, "type": "coin" },
+    { "x": 4500, "y": 280, "type": "coin" },
 
     # section 7
     { "x": 4800, "y": 0, "type": "invisible", "width": 3000, "height": 90 },
@@ -114,6 +150,16 @@ map_data = [
 
     { "x": 5150, "y": 100, "type": "enemy" },
     { "x": 5300, "y": 100, "type": "enemy" },
+
+    { "x": 5120, "y": 210, "type": "coin" },
+    { "x": 5180, "y": 290, "type": "coin" },
+    { "x": 5220, "y": 290, "type": "coin" },
+    { "x": 5280, "y": 210, "type": "coin" },
+
+    { "x": 5480, "y": 280, "type": "coin" },
+    { "x": 5520, "y": 330, "type": "coin" },
+    { "x": 5560, "y": 330, "type": "coin" },
+
     # section 8
     { "x": 5600, "y": 0, "type": "invisible", "width": 520, "height": 90 },
     { "x": 6200, "y": 0, "type": "invisible", "width": 200, "height": 90 },
@@ -145,6 +191,16 @@ map_data = [
 
     { "x": 5820, "y": 100, "type": "enemy" },
 
+    { "x": 5770, "y": 210, "type": "coin" },
+    { "x": 5810, "y": 250, "type": "coin" },
+    { "x": 5850, "y": 250, "type": "coin" },
+    { "x": 5890, "y": 210, "type": "coin" },
+
+    { "x": 6090, "y": 300, "type": "coin" },
+    { "x": 6130, "y": 350, "type": "coin" },
+    { "x": 6170, "y": 350, "type": "coin" },
+    { "x": 6210, "y": 300, "type": "coin" },
+
     # section 9
     { "x": 6400, "y": 0, "type": "invisible", "width": 3000, "height": 90 },
 
@@ -160,6 +216,16 @@ map_data = [
     { "x": 6800, "y": 100, "type": "enemy" },
     { "x": 6900, "y": 100, "type": "enemy" },
     { "x": 7000, "y": 100, "type": "enemy" },
+
+    { "x": 6540, "y": 210, "type": "coin" },
+    { "x": 6580, "y": 260, "type": "coin" },
+    { "x": 6620, "y": 310, "type": "coin" },
+    { "x": 6660, "y": 350, "type": "coin" },
+
+    { "x": 6900, "y": 340, "type": "coin" },
+    { "x": 6940, "y": 380, "type": "coin" },
+    { "x": 6980, "y": 380, "type": "coin" },
+    { "x": 7020, "y": 340, "type": "coin" },
 
     # section 10
     { "x": 7200, "y": 0, "type": "invisible", "width": 3000, "height": 90 },
@@ -184,7 +250,11 @@ map_data = [
     { "x": 7560, "y": 130, "type": "solid", "width": 40, "height": 40},
     { "x": 7560, "y": 90, "type": "solid", "width": 40, "height": 40},
 
-    { "x": 7920, "y": 90, "type": "solid", "width": 40, "height": 40}
+    { "x": 7920, "y": 90, "type": "solid", "width": 40, "height": 40},
 
+    { "x": 7600, "y": 450, "type": "coin" },
+    { "x": 7640, "y": 490, "type": "coin" },
+    { "x": 7680, "y": 530, "type": "coin" },
+    { "x": 7720, "y": 570, "type": "coin" }
 
 ]
